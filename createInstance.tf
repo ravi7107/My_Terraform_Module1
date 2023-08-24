013b340a0046a5ce95a28d0d956a2e12916005cf
ravi7107 <r.r.shte@gmail.com>
2023-08-24 17:09:05 +0530

    create instance file upgraded

--- createInstance.tf
+++ createInstance.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "MyFirstInstance" {
-  ami = "ami-0261755bbcb8c4a84"
-  instance_type = ""
+  ami           = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"  # Replace with your desired instance type
 }
