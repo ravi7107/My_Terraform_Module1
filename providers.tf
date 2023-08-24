6d49e45dab1ff6509817193c04a3ccf81c3316b0
ravi7107 <r.r.shte@gmail.com>
2023-08-24 11:48:52 +0530

    provider block updated

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired AWS region
+  region = "us-east-1"  # Replace with your desired AWS region
+  version = "5.11.0"
   access_key = "YOUR_ACCESS_KEY"
   secret_key = "YOUR_SECRET_KEY"
 }
