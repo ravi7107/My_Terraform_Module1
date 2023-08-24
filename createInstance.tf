6f6a43bfaa55f8dacc308379b997ca747df59e2f
ravi7107 <r.r.shte@gmail.com>
2023-08-24 12:19:59 +0530

    AWS accss_key and secrete_key added

---
+++ createInstance.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "MyFirstInstance" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+}
--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
   version = "5.11.0"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
+  access_key = "AKIA6KE3AQUXKEEAENYQ"
+  secret_key = "lwI8p/oiclWQwkVXkj5KLgpT6SIjmt0bWQIA09nh"
 }
