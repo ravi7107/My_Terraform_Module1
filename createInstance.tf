4563354e75e98e2b93502b19383bc518182f786f
ravi7107 <r.r.shte@gmail.com>
2023-08-24 20:29:34 +0530

    commit3

--- createInstance.tf
+++ createInstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "MyFirstInstance" {
-  ami           = "ami-0261755bbcb8c4a84"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"  # Replace with your desired instance type
 }
 
