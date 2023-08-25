fcfb40ec9c3bb7ce2ba140675e98c1a492d741e4
ravi7107 <r.r.shte@gmail.com>
2023-08-25 12:09:03 +0530

    c15

--- providers.tf
+++ providers.tf
@@ -8,5 +8,4 @@ resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
     count = "${var.number_of_instances}"
     instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
 } 
