f040875813b783897295c01e3bfe9a6b2ce2ab07
ravi7107 <r.r.shte@gmail.com>
2023-08-25 16:33:46 +0530

    c15

--- providers.tf
+++ providers.tf
@@ -1,9 +1,39 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    instance_type = "${var.instance_type}"
-} 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_ami_copy" "ubuntu_encrypted_ami" {
+  name              = "ubuntu-encrypted-ami"
+  description       = "An encrypted root ami based off ${data.aws_ami.ubuntu.id}"
+  source_ami_id     = data.aws_ami.ubuntu.id
+  source_ami_region = "eu-ast-1"
+  encrypted         = true
+
+  tags = { Name = "ubuntu-encrypted-ami" }
+}
+
+data "aws_ami" "encrypted-ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [aws_ami_copy.ubuntu_encrypted_ami.id]
+  }
+
+  owners = ["self"]
+}
