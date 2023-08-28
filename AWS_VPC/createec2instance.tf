0a6ff6f4e86d5e219a264131076ab9a11a1c9870
ravi7107 <r.r.shte@gmail.com>
2023-08-28 19:29:23 +0530

    c5

--- AWS_VPC/createec2instance.tf
+++ AWS_VPC/createec2instance.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "instance_sg" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-024e6efaf93d85776"  # Replace with Ubuntu AMI ID
   instance_type = "t2.micro"
-  subnet_id     = levelup_vpc_public1.id
+  subnet_id     = aws_subnet.levelup_vpc_public1.id
   vpc_security_group_ids = [aws_security_group.allow_levelup_ssh.id]
   
   
