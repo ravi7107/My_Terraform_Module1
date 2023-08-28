f6732dfdf03fb337e4d4e97128096463eeffe744
ravi7107 <r.r.shte@gmail.com>
2023-08-28 18:36:27 +0530

    SG file error removed

--- AWS_VPC/security_group.tf
+++ AWS_VPC/security_group.tf
@@ -10,7 +10,6 @@ resource "aws_security_group" "allow_levelup_ssh" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
