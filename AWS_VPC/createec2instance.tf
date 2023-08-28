2809942f94275f630d8ee387e34766f258300798
ravi7107 <r.r.shte@gmail.com>
2023-08-28 20:18:24 +0530

    c7

--- AWS_VPC/createec2instance.tf
+++ AWS_VPC/createec2instance.tf
@@ -1,12 +1,8 @@
 #creat EC-2 instance 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "hloMnwnopqBw3h9ZnRPycO3IBk+CbQac8okbGUNCCuM r.r.shete@gmail.com"
-}
-
 resource "aws_instance" "my_instance" {
   ami           = "ami-024e6efaf93d85776"  # Replace with Ubuntu AMI ID
   instance_type = "t2.micro"
+  key_name = "demo"
   subnet_id     = aws_subnet.levelup_vpc_public1.id
   vpc_security_group_ids = [aws_security_group.allow_levelup_ssh.id]
   
