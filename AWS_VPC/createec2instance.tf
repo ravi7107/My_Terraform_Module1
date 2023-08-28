73fda497a05da11cee75fe1a3ec99a0b01104c24
ravi7107 <r.r.shte@gmail.com>
2023-08-28 19:26:05 +0530

    c4

--- AWS_VPC/createec2instance.tf
+++ AWS_VPC/createec2instance.tf
@@ -14,12 +14,12 @@ resource "aws_security_group" "instance_sg" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-024e6efaf93d85776"  # Replace with Ubuntu AMI ID
   instance_type = "t2.micro"
-  subnet_id     = "levelup_vpc_public1"
+  subnet_id     = levelup_vpc_public1.id
   vpc_security_group_ids = [aws_security_group.allow_levelup_ssh.id]
   
   
   // Add key_name for SSH access
   
   // Disable public IP assignment
-  associate_public_ip_address = false
+  associate_public_ip_address = true
 }
