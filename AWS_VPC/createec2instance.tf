f0066ee24bf9adefd77bb146d8e092662e93e07b
ravi7107 <r.r.shte@gmail.com>
2023-08-28 23:48:25 +0530

    c14

--- AWS_VPC/createec2instance.tf
+++ AWS_VPC/createec2instance.tf
@@ -1,24 +1,11 @@
-#creat EC-2 instance 
-provider "aws" {
-  region = "us-east-2"
-  access_key = "AKIASAZ4HDPTMBYTXISE"
-  secret_key = "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"
-}
+resource "aws_instance" "web_instance" {
+  ami           = lookup(var.ami_id, var.region)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.custom-vpc-public-subnet-one.id
+  vpc_security_group_ids = [aws_security_group.custom-vpc-security-group.id]
+  key_name      = var.key_name
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-024e6efaf93d85776"  # Replace with Ubuntu AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.levelup_vpc_public1.id
-  vpc_security_group_ids = [aws_security_group.allow_levelup_ssh.id]
-
-user_data = <<EOF
-#!/bin/bash
-echo "Copying the SSH Key to the server"
-echo -e "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyfTbQ5TOhb7iiPh6eeAlHWMdhs9gU+sEOGyD0Kh6h+nEoJsn2PrpN1W9PLtVotfKmy7OdaJPAXlwbqkh5ucE13F7FZw8ebt1juq26it4OvCIWvxCAeIPCv9pXOD8Zxd13d6hgvKG3/KwJ7d6InYqLZaQWRqLpnCvhRFSjiZhJZYYEzVkkStLSZia2TRgmv0O98yfo+9wkNwjBYGs7IDSJDFwT4IOskKL+XRcPYwqs4+GRLgejj961aAIdKuhxyS1XlPP6PVrpsXAK7mW8sSX4PN2zSntnApOT+IYLkkmmzoZKAjgSSdVY/4Vj3HJa3xIfLjNkX9iuGC5FpvPvizxz97ZnK8oiwUOCzsgdcIkOTf2Bx8DYx8JMYPubkk6eS6PA7SPFmc9KLjTvmmRg4AbeVB31P+BWIhLmZBkeEDLlk77tq9thWIxVT/0h3mXze5AV0yoNM0hqmUToGmO5EX3gDL9O7GcwF/W3Ve4SPl6NLvgmVVEYIZ5eQiTqn7audfDRMTn6DGlRIeca9NU6f4p93GKoMBym9Z7tbGuFA0xcZeIkaunEGtoFEo7ObLYeSMdaRqZBQ1gE5CDCzBv3BV0Z2de+1kP/4UPbu7iQvEyjehkGwA+k9fe7g718aGSVJ2ZFCcdU6eaoGjNjeCCUpiTVcODazBwwM0BQo7PX0Rg1lw== r.r.shete@gmail.com"
- EOF 
-  
-  // Add key_name for SSH access
-  
-  // Disable public IP assignment
-  associate_public_ip_address = true
+  tags = {
+    Name = var.ec2_tags
+  }
 }
