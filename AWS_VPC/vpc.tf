5abcdbb08dc0e96bcdbb159db6277253bc18e2d6
ravi7107 <r.r.shte@gmail.com>
2023-08-28 15:50:20 +0530

    ommit-4

--- AWS_VPC/vpc.tf
+++ AWS_VPC/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "levelup_vpc" {
   instance_tenancy = "default"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink = "false"
+  #enable_classiclink = "false"
 
   tags = {
     Name = "levelup_vpc"
