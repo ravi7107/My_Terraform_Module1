ee61e7fc9ea3ccc2f2b35c35f0b71bca0e0d3f7b
ravi7107 <r.r.shte@gmail.com>
2023-08-29 16:47:24 +0530

    ec2 instance creation

--- EC-2 instance/ec2instance.tf
+++ EC-2 instance/ec2instance.tf
@@ -2,21 +2,13 @@ provider "aws" {
   region = "us-east-2a"  # Change to your desired region
 }
 
-resource "aws_security_group" "instance_sg" {
-  name_prefix = "instance-sg-"
-  vpc_id      = aws_vpc.levelup_vpc.id
-  
-  // Define inbound and outbound rules here
-}
+resource "aws_instance" "web_instance" {
+  ami           = lookup(var.ami_id, var.region)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.custom-vpc-public-subnet-one.id
+  key_name      = var.key_name
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-024e6efaf93d85776"  # Replace with Ubuntu AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = "levelup_vpc_public1"
-  
-  
-  // Add key_name for SSH access
-  
-  // Disable public IP assignment
-  associate_public_ip_address = false
+  tags = {
+    Name = var.ec2_tags
+  }
 }
