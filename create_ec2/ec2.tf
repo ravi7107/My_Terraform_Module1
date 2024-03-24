e2d86bbe716c5d0e6d5c90ccaf6618e78ea079a8
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-24 20:47:59 +0000

    changes to key pair sync

--- create_ec2/ec2.tf
+++ create_ec2/ec2.tf
@@ -3,3 +3,8 @@ resource "aws_instance" "first_instance" {
   instance_type           = "t2.micro"
   key_name = var.my_tf_key
 }
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.my_tf_key
+  public_key = file("${abspath(path.cwd)}/my_tf_key.pub")
+}
