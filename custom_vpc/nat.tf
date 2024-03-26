11e68d73848aa3ba0fcc33957fef5fb953be2fcd
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-26 15:37:02 +0000

    ok

--- custom_vpc/nat.tf
+++ custom_vpc/nat.tf
@@ -1,12 +1,3 @@
-resource "aws_route_table" "test" {
-  vpc_id = aws_vpc.test.id
-
-  # since this is exactly the route AWS will create, the route will be adopted
-  route {
-    cidr_block = "10.1.0.0/16"
-    gateway_id = "local"
-  }
-}
 
 resource "aws_route_table" "test" {
   vpc_id = aws_vpc.test.id
