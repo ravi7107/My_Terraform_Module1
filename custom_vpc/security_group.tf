a35aa8caae5716227320f3024adc6870b515a7ce
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-26 11:35:19 +0000

    ok

--- custom_vpc/security_group.tf
+++ custom_vpc/security_group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "custom-vpc-security-group" {
   name        = "custom-vpc-security-group"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.levelup_vpc.id
 
   tags = {
     Name = "custom-vpc-security-group"
@@ -10,22 +10,22 @@ resource "aws_security_group" "custom-vpc-security-group" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.custom-vpc-security-group.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc.levelup_vpc.cidr_block.id
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  security_group_id = aws_security_group.custom-vpc-security-group.id
+  cidr_ipv6         = aws_vpc.levelup_vpc.ipv6_cidr_block.id
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.custom-vpc-security-group.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
