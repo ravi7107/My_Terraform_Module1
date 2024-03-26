9d0e6eaa30b740ffccfd7c8554e2549999769178
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-26 16:09:52 +0000

    new vpc code

--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -1,30 +1,8 @@
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
- 
- tags = {
-   Name = "Project VPC"
- }
-}
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-resource "aws_subnet" "public_subnets" {
- count             = length(var.public_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
- cidr_block        = element(var.public_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
- 
- tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
+  tags = {
+    Name = "main"
+  }
 }
- 
-resource "aws_subnet" "private_subnets" {
- count             = length(var.private_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
- cidr_block        = element(var.private_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
- 
- tags = {
-   Name = "Private Subnet ${count.index + 1}"
- }
-}
-
