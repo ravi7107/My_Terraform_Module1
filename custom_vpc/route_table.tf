9aad0c042605075e5d7686e53c955a61bf7c2ee0
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-26 16:45:54 +0000

    RT code changes

--- custom_vpc/route_table.tf
+++ custom_vpc/route_table.tf
@@ -1,10 +1,9 @@
-
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.route_table.id
 
   # since this is exactly the route AWS will create, the route will be adopted
   route {
-    cidr_block = aws_vpc.route_table.cidr_block
+    cidr_block = "10.1.0.0/16"
     gateway_id = "local"
   }
 }
