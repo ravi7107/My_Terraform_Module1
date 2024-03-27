ca9cfa8a785ba0b90dde27676606d36984fd4762
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-27 04:04:46 +0000

    ok

--- custom_vpc/route_table.tf
+++ custom_vpc/route_table.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table" "test" {
-  vpc_id = aws_vpc.test.id
+  vpc_id = aws_vpc.main.id
 
   # since this is exactly the route AWS will create, the route will be adopted
   route {
