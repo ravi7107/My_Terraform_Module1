98ed8b7975e0cf6b8820525bf692740e190f02bd
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-28 05:04:46 +0000

    c5

--- user-data/variables.tf
+++ user-data/variables.tf
@@ -1,12 +1,9 @@
 
 
 variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0c101f26f147fa7fd"
-        us-east-2 = "ami-019f9b3318b7155c5"
-        us-east-2 = "ami-019f9b3318b7155c5"
-    }
+    type = string
+    default = us-east-1 = "ami-0c101f26f147fa7fd"
+       
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
