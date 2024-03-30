e9c2f25d49c5aab9632f5e042a53bd67b6b9b609
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-30 10:25:37 +0000

    ok

--- Terraform for AWS cloud 2/AWS RDS/create_ec2.tf
+++ Terraform for AWS cloud 2/AWS RDS/create_ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "first_instance" {
-  ami                     = "ami-0c101f26f147fa7fd"
+  ami                     = "ami-019f9b3318b7155c5"
   instance_type           = "t2.micro"
   availability_zone       ="us-east-2a" 
   key_name = "var.levelup_key"
