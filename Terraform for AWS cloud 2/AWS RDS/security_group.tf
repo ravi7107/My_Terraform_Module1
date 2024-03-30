bd02b6328488144d146cc9c2479f8dfbbd22ae60
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-30 10:22:13 +0000

    ok

--- Terraform for AWS cloud 2/AWS RDS/security_group.tf
+++ Terraform for AWS cloud 2/AWS RDS/security_group.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
