25ebd659b67d735df755957a09e47cf741c79ddf
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-31 15:42:13 +0000

    c3

--- Terraform for AWS cloud 2/AWS RDS/private_subnet.tf
+++ Terraform for AWS cloud 2/AWS RDS/private_subnet.tf
@@ -2,6 +2,7 @@
 resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "private_subnet_1"
@@ -11,6 +12,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "private_subnet_2"
