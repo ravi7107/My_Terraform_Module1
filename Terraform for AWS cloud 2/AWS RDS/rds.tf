082323cb8ea0c977532c0ea335a6ad0d37f8086c
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-31 17:04:09 +0000

    c9

--- Terraform for AWS cloud 2/AWS RDS/rds.tf
+++ Terraform for AWS cloud 2/AWS RDS/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "default" {
   storage_type = "gp2"
   engine = "mysql"
   engine_version = "5.7"
-  instance_class = "db.t2.micro"
+  instance_class = "db.t3.small"
   identifier = "mydb"
   username = "dbuser"
   password = "dbpassword"
