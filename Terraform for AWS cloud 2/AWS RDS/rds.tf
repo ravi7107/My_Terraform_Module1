4c758775bc457b2513eaf00f2622ec645adbec35
ravi7107 <131547170+ravi7107@users.noreply.github.com>
2024-03-31 15:52:08 +0000

    c7

--- Terraform for AWS cloud 2/AWS RDS/rds.tf
+++ Terraform for AWS cloud 2/AWS RDS/rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "default" {
   username = "dbuser"
   password = "dbpassword"
 
-  vpc_security_group_ids = [aws_security_group.rds_sg]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
 
   skip_final_snapshot = true
