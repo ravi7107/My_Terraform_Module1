2beb6949e4a89e5bba880427dd01baa7cd3fd5a1
ravi7107 <r.r.shete@gmail.com>
2024-04-03 10:43:50 +0530

    c2

--- ec2-autoscaling/create-instance.tf
+++ ec2-autoscaling/create-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Server-1" {
   ami           = lookup(var.ami_id, var.region)
   instance_type = var.instance_type
   #subnet_id     = aws_subnet.custom-vpc-public-subnet-one.id
-  key_name      = var.key_name
+  key_name      = var.levelup_key
 
   tags = {
     Name = "Server-1"
