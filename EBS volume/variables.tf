742e58e7769ac6ac2843653dd14689e801598533
ravi7107 <r.r.shete@gmail.com>
2024-04-03 09:20:55 +0530

    ok

--- EBS volume/variables.tf
+++ EBS volume/variables.tf
@@ -1,13 +1,4 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASAZ4HDPTIDSWCUOX"
-}
 
-variable "AWS_SECRET_KEY" {}
-
-variable "AWS_REGION" {
-default = "us-east-2"
-}
 
 variable "AMIS" {
     type = map
