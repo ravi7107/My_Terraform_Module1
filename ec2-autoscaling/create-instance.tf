68f21a247e510d196acd4030e67325a704b68dce
ravi7107 <r.r.shete@gmail.com>
2024-04-03 13:07:27 +0530

    a5

--- ec2-autoscaling/create-instance.tf
+++ ec2-autoscaling/create-instance.tf
@@ -1,15 +1,16 @@
-resource "aws_instance" "Server-1" {
-  ami           = lookup(var.ami_id, var.region)
-  instance_type = var.instance_type
-  #subnet_id     = aws_subnet.custom-vpc-public-subnet-one.id
-  key_name      = var.levelup_key
+resource "aws_instance" "first_instance" {
+  ami                     = "ami-019f9b3318b7155c5"
+  instance_type           = "t2.micro"
+  availability_zone       ="us-east-2a" 
+  key_name = "var.levelup_key"
+ 
 
   tags = {
-    Name = "Server-1"
+    Name= "My first instance"
   }
 }
 
-resource "aws_key_pair" "my_key" {
-  key_name   = var.key_name
-  public_key = file("${abspath(path.cwd)}/levelup_key_pub")
+resource "aws_key_pair" "levelup_key" {
+  key_name   = "var.levelup_key"
+  public_key = file("${abspath(path.cwd)}/levelup_key.pub")
 }
