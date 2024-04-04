c046c36df6dec3668a0daaf9436b0010269d2061
ravi7107 <r.r.shete@gmail.com>
2024-04-04 13:09:13 +0530

    ok

--- ec2-autoscaling/variables.tf
+++ ec2-autoscaling/variables.tf
@@ -1,12 +1,29 @@
-variable "ami_id" {
-  description = "Specify the instance ami that you want to use for the instance."
-  type        = map
+variable "multi_az_config" {
+  description = "Specify configurations for multiple availability zones"
+  type        = map(object({
+    instance_type  = string
+    ami_id         = string
+    key_name       = string
+    key_name_pub   = string
+    # Add more configuration options as needed
+  }))
   default     = {
-     "us-east-2" = "ami-0900fe555666598a2"
-     "us-west-1" = "ami-0b990d3cfca306617"
-      "default" =  "ami-0900fe555666598a2"
+    "us-east-2" = {
+      instance_type  = "t2.micro"
+      ami_id         = "8ami-0900fe555666598a2"
+      key_name       = "levelup_key"
+      key_name_pub   = "levelup_key.pub"
+    },
+    "us-west-1" = {
+      instance_type  = "t2.micro"
+      ami_id         = "ami-0b990d3cfca306617"
+      key_name       = "levelup_key"
+      key_name_pub   = "levelup_key.pub"
+    },
+    # Add more availability zones and their configurations as needed
   }
 }
+
 
 variable "region" {
   type    = map(string)
