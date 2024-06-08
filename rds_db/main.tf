6cba6ee366733e0609f9c6cb7d50b97d45de8a9f
ravi7107 <r.r.shete@gmail.com>
2024-06-08 19:46:04 +0530

    db name code changes

--- rds_db/main.tf
+++ rds_db/main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "default" {
   engine                 = var.db_engine
   engine_version         = var.db_engine_version
   instance_class         = var.db_instance_class
-  name                   = var.db_name
+  db_name                = var.db_name 
   username               = var.db_username
   password               = var.db_password
   parameter_group_name   = "default.${var.db_engine}${var.db_engine_version}"
