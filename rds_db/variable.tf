4a874827543ffea8aa207fd8152451ac90959824
ravi7107 <r.r.shete@gmail.com>
2024-06-08 19:42:00 +0530

    rds database added

---
+++ rds_db/variable.tf
@@ -0,0 +1,45 @@
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t2.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "The allocated storage for the RDS instance"
+  type        = number
+  default     = 20
+}
+
+variable "db_engine" {
+  description = "The database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "db_engine_version" {
+  description = "The database engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "db_backup_retention_period" {
+  description = "The number of days to retain backups"
+  type        = number
+  default     = 7
+}
