3e4bb95e0474c24d697232092b67892374717605
ravi7107 <r.r.shete@gmail.com>
2024-06-09 10:35:05 +0530

    30

--- static-website-hosting-1/route53.tf
+++ static-website-hosting-1/route53.tf
@@ -5,24 +5,23 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_s3_bucket.website_bucket.website_endpoint]
 }
 
 resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_s3_bucket.website_bucket.website_endpoint]
 }
 
+resource "aws_route53_record" "ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "NS"
+  ttl     = 300
+  records = aws_route53_zone.main.name_servers
+}
