aaa7a547a1a4edca81358d6a6b67ac014fadda27
ravi7107 <r.r.shete@gmail.com>
2024-06-09 09:57:45 +0530

    26

--- static-website-hosting-1/variables.tf
+++ static-website-hosting-1/variables.tf
@@ -1,6 +1,35 @@
 variable "region" {
-  default = "us-east-1"
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"
 }
+
+variable "zone_map" {
+  description = "Map of regions to Route 53 hosted zone IDs"
+  type = map(object({
+    domain_name = string
+    zone_id     = string
+  }))
+  default = {
+    "us-east-1" = {
+      domain_name = "example.com"
+      zone_id     = "Z3M3LMPEXAMPLE"
+    },
+    "us-west-2" = {
+      domain_name = "example-west.com"
+      zone_id     = "Z2LMNOPEXAMPLE"
+    }
+  }
+}
+
+variable "current_zone" {
+  description = "Current zone settings based on the selected region"
+  type = object({
+    domain_name = string
+    zone_id     = string
+  })
+  default = var.zone_map[var.region]
+}
+
 
 variable "domain_name" {
   description = "The domain name to associate with this site"
